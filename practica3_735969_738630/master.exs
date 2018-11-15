9e4748fcc9931b54b140b98792b6a2f1c23daa48
David <davidsolanas086@gmail.com>
2018-11-15 23:36:58 +0100

    pp

--- practica3_735969_738630/master.exs
+++ practica3_735969_738630/master.exs
@@ -24,16 +24,16 @@ end
 #Calcula los pares de numeros amigos desde num1 hasta n
 defp masterAux(workerSDP, workerDiv, workerSuma, num1, listaPares, c_pid) do
 	IO.puts(num1)
-	sumaA = action(10000, workerSDP, workerDiv, workerSuma, c_pid, 0, num1, num1,0)
-	sumaB = action(10000, workerSDP, workerDiv, workerSuma, c_pid, 0, num1, sumaA,0)
+	{sumaA, workerSDPA, workerDivA, workerSumaA} = action(10000, workerSDP, workerDiv, workerSuma, c_pid, 0, num1, num1,0)
+	{sumaB, workerSDPB, workerDivB, workerSumaB} = action(10000, workerSDPA, workerDivA, workerSumaA, c_pid, 0, num1, sumaA,0)
 	#Comprueba que los dos numeros sean amigos (sumaA y num1) y los añade en la lista si no estaba ya el par
 	if sumaB == num1 and sumaA != num1 and not(Enum.member?(listaPares,{sumaA,num1})) do
 		IO.puts (num1)
 		IO.puts(sumaA)
 		IO.puts("---------------")
-	 	masterAux(workerSDP, workerDiv, workerSuma, num1+1, listaPares++[{num1,sumaA}], c_pid) 
+	 	masterAux(workerSDPB, workerDivB, workerSumaB, num1+1, listaPares++[{num1,sumaA}], c_pid) 
 	else 
-	 	masterAux(workerSDP, workerDiv, workerSuma, num1+1, listaPares, c_pid)
+	 	masterAux(workerSDPB, workerDivB, workerSumaB, num1+1, listaPares, c_pid)
 	end
 end
 
@@ -49,39 +49,41 @@ def action(timeout,workerSDP, workerDiv, workerSuma, c_pid, retry, idOperaciones
 		{:replyDiv, divisores, idOp, workerDiv_new} ->
 			case idOp do
 				-1 -> send(workerDiv_new, {:reqWorkerDiv, {self(), num, idOperaciones}})
-						action(timeout,workerSDP, workerDiv_new, workerSuma,c_pid,retry,idOperaciones,num,1)
+						{sumaDP, wSDP, wDiv, wSum}=action(timeout,workerSDP, workerDiv_new, workerSuma,c_pid,retry,idOperaciones,num,1)
+						{sumaDP, wSDP, workerDiv_new, wSum}
 				idOperaciones -> send(workerSuma, {:reqWorkerSuma, {self(), divisores, idOperaciones}})
-								sum = receive do
+								res = receive do
 									{:replySum, suma, idOp, workerSuma_new} -> 
 																	case idOp do
 																		-1 -> send(workerSuma_new, {:reqWorkerSuma, {self(), divisores, idOperaciones}})
-																				action(timeout,workerSDP, workerDiv, workerSuma_new,c_pid,retry,idOperaciones,num,2)
-																		idOperaciones -> suma
+																				{sumaDP, wSDP, wDiv, wSum}=action(timeout,workerSDP, workerDiv, workerSuma_new,c_pid,retry,idOperaciones,num,2)
+																				{sumaDP, wSDP, wDiv, workerSuma_new}
+																		idOperaciones -> {suma, workerSDP, workerDiv, workerSuma}
 																		_ -> action(timeout,workerSDP, workerDiv, workerSuma,c_pid,retry,idOperaciones,num,2)
 																	end
 																	
 									#Compruebo si SDP ha acabado también, si ha acabado, eligo esta opción para no retrasar al cliente
 									{:replySDP, sumDivisoresProp, idOp, workerSDP_new} -> 
 																case idOp do
-																	-1 -> action(timeout,workerSDP_new, workerDiv, workerSuma,c_pid,retry,idOperaciones,num,0)
-																	idOperaciones -> sumDivisoresProp
+																	-1 -> {sumaDP, wSDP, wDiv, wSum}=action(timeout,workerSDP_new, workerDiv, workerSuma,c_pid,retry,idOperaciones,num,0)
+																			{sumaDP, workerSDP_new, wDiv, wSum}
+																	idOperaciones -> {sumDivisoresProp, workerSDP, workerDiv, workerSuma}
 																	_ -> action(timeout,workerSDP, workerDiv, workerSuma,c_pid,retry,idOperaciones,num,2)
 																end
 
 								after
 									timeout -> action(timeout*(trunc(:math.pow(2,retry))), workerSDP, workerDiv, workerSuma, c_pid, retry+1, idOperaciones, num, 0) #Vuelve a intentar
 								end
-								sum
+								res
 				_ -> action(timeout,workerSDP, workerDiv, workerSuma,c_pid,retry,idOperaciones,num,1)
 			end
 				
-		{:replySDP, sumDivisoresProp, idOp, workerSDP_new} -> IO.puts("Recibido:")
-																IO.puts(idOp)
-																IO.puts("-----------")
+		{:replySDP, sumDivisoresProp, idOp, workerSDP_new} -> 
 													case idOp do
 														-1 -> send(workerSDP_new, {:reqWorkerSDP, {self(), num, idOperaciones}})	
-															action(timeout,workerSDP_new, workerDiv, workerSuma,c_pid,retry,idOperaciones,num,1)
-														idOperaciones -> sumDivisoresProp
+															{sumaDP, wSDP, wDiv, wSum}=action(timeout,workerSDP_new, workerDiv, workerSuma,c_pid,retry,idOperaciones,num,1)
+															{sumaDP, workerSDP_new, wDiv, wSum}
+														idOperaciones -> {sumDivisoresProp, workerSDP, workerDiv, workerSuma}
 														_ -> action(timeout,workerSDP, workerDiv, workerSuma,c_pid,retry,idOperaciones,num,1)
 													end
 	after
--- practica3_735969_738630/worker.exs
+++ practica3_735969_738630/worker.exs
@@ -107,7 +107,7 @@ defmodule Worker do
   defp loopI(worker_type, which_worker, lista, pid_master) do
     IO.puts(worker_type)
     delay = case worker_type do
-      :crash -> if :rand.uniform(100) > 75, do: :infinity, else: 0
+      :crash -> if :rand.uniform(100) > 75, do: 5000, else: 0
       :timing -> :rand.uniform(100)*10 #00
       _ ->  0
     end
