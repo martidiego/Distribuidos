1f4f1ad06b11ac69b30847a014bf8ca98cbb2a12
David <davidsolanas086@gmail.com>
2018-11-15 17:48:46 +0100

    Update practica3_735969_738630/master.exs

--- practica3_735969_738630/master.exs
+++ practica3_735969_738630/master.exs
@@ -8,88 +8,81 @@
 defmodule Master do
 
 
-def master(worker_div, worker_suma, worker_sdp) do
+def master(workers) do
 	receive do
-		{c_pid, num1, :req} -> masterAux(worker_div, worker_suma, worker_sdp, num1, [], c_pid)
+		{c_pid, num1, :req} -> masterAux(workers, num1, [], c_pid)
 	end
-	master(worker_div, worker_suma, worker_sdp)
+	master(workers)
 end
 
 
-defp masterAux(worker_div, worker_suma, worker_sdp, num1, listaPares, c_pid) when num1 > 20000 do
+defp masterAux(workers, num1, listaPares, c_pid) when num1 > 20000 do
 	send(c_pid, {:reply, listaPares})
 end
 
 
 #Calcula los pares de numeros amigos desde num1 hasta n
-defp masterAux(worker_div, worker_suma, worker_sdp, num1, listaPares, c_pid) do
+defp masterAux(workers, num1, listaPares, c_pid) do
 	IO.puts(num1)
-	sumaA = action(10000, worker_div, worker_suma, worker_sdp, c_pid, 0, num1, num1,0)
-	sumaB = action(10000, worker_div, worker_suma, worker_sdp, c_pid, 0, num1, sumaA,0)
+	sumaA = action(10000, workers, c_pid, 0, num1, num1,0)
+	sumaB = action(10000, workers, c_pid, 0, num1, sumaA,0)
 	#Comprueba que los dos numeros sean amigos (sumaA y num1) y los añade en la lista si no estaba ya el par
 	if sumaB == num1 and sumaA != num1 and not(Enum.member?(listaPares,{sumaA,num1})) do
 		IO.puts (num1)
 		IO.puts(sumaA)
 		IO.puts("---------------")
-	 	masterAux(worker_div, worker_suma, worker_sdp, num1+1, listaPares++[{num1,sumaA}], c_pid) 
+	 	masterAux(workers, num1+1, listaPares++[{num1,sumaA}], c_pid) 
 	else 
-	 	masterAux(worker_div, worker_suma, worker_sdp, num1+1, listaPares, c_pid)
+	 	masterAux(workers, num1+1, listaPares, c_pid)
 	end
 end
 
 
-recibir_mensajes (timeout,worker_div, worker_suma, worker_sdp, c_pid, retry, idOperaciones, num) do
-	receive do
-		{:replyDiv, divisores, idOp, worker_div} ->
+def action(timeout,workers, c_pid, retry, idOperaciones, num, yaMandado) when retry < 5 do
+	if yaMandado == 0 do
+		send(Enum.at(workers,2), {:reqWorkerSDP, {self(), num, idOperaciones}})	#Envia al worker suma_divisores_propios la peticion
+		send(hd(workers), {:reqWorkerDiv, {self(), num, idOperaciones}})	#Envia al worker divisores_propios la peticion
+	end
+	result = receive do
+		{:replyDiv, divisores, idOp} ->
 				if (idOp == idOperaciones) do
-					send(worker_sum, {:reqWorkerSuma, {self(), divisores, idOperaciones}})
+					send(Enum.at(workers,1), {:reqWorkerSuma, {self(), divisores, idOperaciones}})
 					sum = receive do
-						{:replySuma, suma, idOp, worker_suma} -> 	
-							if idOp == idOperaciones do
-							 	suma 
-							else
-								if idOp == -1, do: send(worker_suma, {:reqWorkerSuma, {self(), num, idOperaciones}})	
-								recibir_mensajes(timeout,worker_div, worker_suma, worker_sdp,c_pid,retry,idOperaciones,num)
-							end
+						{:replySuma, suma, idOp} -> 	
+														if idOp == idOperaciones do
+														 	suma 
+														else
+															action(timeout,workers,c_pid,retry,idOperaciones,num,0)
+														end
 						#Compruebo si SDP ha acabado también, si ha acabado, eligo esta opción para no retrasar al cliente
-						{:replySDP, sumDivisoresProp, idOp, worker_sdp} -> 		
-							if idOp == idOperaciones do
-							 	sumDivisoresProp 
-							else
-							 	if idOp == -1, do : send(worker_sdp, {:reqWorkerSDP, {self(), num, idOperaciones}})
-							 	recibir_mensajes(timeout,worker_div, worker_suma, worker_sdp,c_pid,retry,idOperaciones,num)
-							end
+						{:replySDP, sumDivisoresProp, idOp} -> 		
+													if idOp == idOperaciones do
+													 	sumDivisoresProp 
+													else
+													 	action(timeout,workers,c_pid,retry,idOperaciones,num,0)
+													end
 
-						after
-							timeout -> action(timeout*(trunc(:math.pow(2,retry))), worker_div, worker_suma, worker_sdp, c_pid, retry+1, idOperaciones, num, 0) #Vuelve a intentar
-						end
-						sum	
+					after
+						timeout -> action(timeout*(trunc(:math.pow(2,retry))), workers, c_pid, retry+1, idOperaciones, num, 0) #Vuelve a intentar
+					end
+					sum
 				else
-					if idOp == -1, do: send(worker_div, {:reqWorkerDiv, {self(), num, idOperaciones}})				
-					recibir_mensajes(timeout,worker_div, worker_suma, worker_sdp,c_pid,retry,idOperaciones,num)
+					#Descarta respuesta recibida
+					action(timeout,workers,c_pid,retry,idOperaciones,num,1)
 				end
 				
-		{:replySDP, sumDivisoresProp, idOp, worker_sdp} -> 		
-			if idOp == idOperaciones do
-			 	sumDivisoresProp 
-			else 
-				#Descarta operacion recibida
-				if idOp == -1, do: send(worker_sdp, {:reqWorkerSDP, {self(), num, idOperaciones}})	
-			 	recibir_mensajes(timeout,worker_div, worker_suma, worker_sdp,c_pid,retry,idOperaciones,num)
-			end
-
-		after
-			timeout -> action(timeout*(trunc(:math.pow(2,retry))), worker_div, worker_suma, worker_sdp, c_pid, retry+1, idOperaciones, num, 0) #Vuelve a intentar
-		end
-    end
-
-def action(timeout,worker_div, worker_suma, worker_sdp, c_pid, retry, idOperaciones, num, yaMandado) when retry < 5 do
-	if yaMandado == 0 do
-		send(worker_sdp, {:reqWorkerSDP, {self(), num, idOperaciones}})	#Envia al worker suma_divisores_propios la peticion
-		send(worker_div, {:reqWorkerDiv, {self(), num, idOperaciones}})	#Envia al worker divisores_propios la peticion
+		{:replySDP, sumDivisoresProp, idOp} -> 		
+													if idOp == idOperaciones do
+													 	sumDivisoresProp 
+													else
+														#Descarta operacion recibida
+													 	action(timeout,workers,c_pid,retry,idOperaciones,num,1)
+													end
+	after
+		timeout -> action(timeout*(trunc(:math.pow(2,retry))), workers, c_pid, retry+1, idOperaciones, num, 0) #Vuelve a intentar
 	end
-	recibir_mensajes(timeout,worker_div, worker_suma, worker_sdp, c_pid, retry, idOperaciones, num)
-	end
+	result
+end
 
 #Si se llega al máximo numero de intentos se acaba la ejecución
 def action(timeout,workers, c_pid, retry, idOperaciones, num, yaMandado) when retry == 5 do
